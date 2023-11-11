8efb382c7fbf0f857005fc06226c20df80bcd0ad
pecet3 <pecet3107@gmail.com>
2023-11-11 19:02:41 +0100

    config initail

---
+++ backend/models/config.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var db *sql.DB
+
+func Config() {
+	db, err := sql.Open("sqlite3", "./db1.db")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	statement, err := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS posts (
+		id INTEGER PRIMARY KEY, 
+		user_id INTEGER, 
+		title TEXT,
+		content TEXT,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	statement.Exec()
+}
