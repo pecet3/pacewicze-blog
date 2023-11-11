7b071019dd8a97c4508e2bb13055c0689033153b
pecet3 <pecet3107@gmail.com>
2023-11-12 00:45:43 +0100

    cleaned up

--- backend/main.go
+++ backend/main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"backend/models"
 	router "backend/routes"
-	"fmt"
 )
 
 func main() {
 	models.Config()
-	post := &models.Post{}
-
-	post, _ = post.CreateAPost()
-	r, _ := models.GetAllPosts()
-	fmt.Println(r)
 	router.SetupAndRun()
 }
