4a9239c6122d84f392d79fb2b2636385db766029
pecet3 <pecet3107@gmail.com>
2023-11-12 00:45:53 +0100

    post method in post nahdling

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -2,22 +2,38 @@ package controllers
 
 import (
 	"backend/models"
+	"backend/utils"
 	"encoding/json"
 	"log"
 	"net/http"
 )
 
-func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+func Post(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
 
-	posts, err := models.GetAllPosts()
-	res, err := json.Marshal(posts)
+	if method == "GET" {
+		posts, err := models.GetAllPosts()
+		res, err := json.Marshal(posts)
 
-	if err != nil {
-		log.Fatalln("error get all posts: ", err)
-		return
+		if err != nil {
+			log.Fatalln("error get all posts: ", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	if method == "POST" {
+		post := &models.Post{}
+		utils.ParseJsonBody(r, post)
+		post, err := post.CreateAPost()
+		if err != nil {
+			log.Fatalln("error create post: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}
 }
