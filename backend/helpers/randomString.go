9b45951b11a8ba0fee6ac8c2cb5c07ec3d9e8e15
pecet3 <pecet3107@gmail.com>
2023-11-11 22:34:00 +0100

    initial

---
+++ backend/helpers/randomString.go
@@ -0,0 +1,16 @@
+package helpers
+
+import (
+	"math/rand"
+)
+
+func generateRandomString(length int) string {
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(result)
+}
