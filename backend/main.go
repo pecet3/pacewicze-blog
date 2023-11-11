bba581375e468891e3e29069bfd7c8f76d129a0f
pecet3 <pecet3107@gmail.com>
2023-11-11 20:56:39 +0100

    cleanded up

--- backend/main.go
+++ backend/main.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"database/sql"
+	"backend/models"
+	router "backend/routes"
 	"fmt"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
-type Post struct {
-}
-
 func main() {
-	db, err := sql.Open("sqlite3", "./db1.db")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("hello")
-	statement, err := db.Prepare(`CREATE TABLE IF NOT EXISTS posts (
-		id INTEGER PRIMARY KEY, 
-		user_id INTEGER, 
-		title TEXT,
-		content TEXT,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`)
+	models.Config()
+	post := &models.Post{}
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	statement.Exec()
-
-	statement, err = db.Prepare(`INSERT INTO posts (user_id, title, content) VALUES (?,?,?)`)
-	if err != nil {
-		fmt.Println(err)
-	}
-	statement.Exec(1, "test", "lorem ispum hello world")
-
-	rows, err := db.Query("SELECT content FROM posts")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var content string
-	for rows.Next() {
-		rows.Scan(&content)
-		defer rows.Close()
-		fmt.Println(content)
-	}
+	post, _ = post.CreateAPost()
+	r, _ := models.GetAllPosts()
+	fmt.Println(r)
+	router.SetupAndRun()
 }
