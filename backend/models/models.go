c5ce448b5fa555826197286120f1cf6117b37612
pecet3 <pecet3107@gmail.com>
2023-11-11 20:57:02 +0100

    initial

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Post struct {
 	Id        uint64    `json:"id"`
@@ -8,4 +11,42 @@ type Post struct {
 	Title     string    `json:"title"`
 	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func GetAllPosts() ([]Post, error) {
+	var posts []Post
+
+	statement := "SELECT * FROM posts"
+
+	rows, err := db.Query(statement)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var post Post
+		err := rows.Scan(&post.Id, &post.UserId, &post.Title, &post.Content, &post.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func (p *Post) CreateAPost() (*Post, error) {
+	statement := "INSERT INTO posts (user_id, title, content) VALUES (?,?,?)"
+	fmt.Println(p)
+	_, err := db.Query(statement, 1, "title", "lorem ipsum")
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return p, nil
 }
