87399a98b7d3d93348ba7a9335abfc0a93804414
pecet3 <pecet3107@gmail.com>
2023-11-12 00:46:42 +0100

    renamed controller

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -12,7 +12,7 @@ func SetupAndRun() {
 	mux := mux.NewRouter()
 	port := ":5000"
 
-	mux.HandleFunc("/post", controllers.GetAllPosts).Methods("GET")
+	mux.HandleFunc("/post", controllers.Post).Methods("GET", "POST")
 	log.Println("Starting the server at port", port)
 	log.Fatal(http.ListenAndServe(port, mux))
 }
