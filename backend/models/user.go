6bddbefe08abf13a9a3e8cabcafa62f55b45a567
pecet3 <pecet3107@gmail.com>
2023-11-12 21:58:48 +0100

    user type

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type User struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	ImageUrl  string    `json:"image_url"`
+	CreatedAt time.Time `json:"created_at"`
+}
