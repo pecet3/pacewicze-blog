05e16d0f0e26403e13e843e1704d6c6b528f623b
pecet3 <pecet3107@gmail.com>
2023-11-12 14:40:01 +0100

    added controller postbyid

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Post(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +18,8 @@ func Post(w http.ResponseWriter, r *http.Request) {
 		res, err := json.Marshal(posts)
 
 		if err != nil {
-			log.Fatalln("error get all posts: ", err)
+			log.Println("error get all posts: ", err)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
@@ -30,10 +33,41 @@ func Post(w http.ResponseWriter, r *http.Request) {
 		utils.ParseJsonBody(r, post)
 		post, err := post.CreateAPost()
 		if err != nil {
-			log.Fatalln("error create post: ", err)
+			log.Println("error create post: ", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(http.StatusOK)
 	}
+}
+
+func PostById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	postId := vars["id"]
+
+	post, err := models.GetPostById(postId)
+
+	if post == (models.Post{}) {
+		log.Println("error get post by id: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err != nil {
+		log.Println("error get post by id: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(post)
+
+	if err != nil {
+		log.Println("error parsing json: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+
 }
