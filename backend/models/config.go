4dafb7ee013bda44c0ff42122c77e85636949ef6
pecet3 <pecet3107@gmail.com>
2023-11-15 00:16:30 +0100

    DODANO SÓL

--- backend/models/config.go
+++ backend/models/config.go
@@ -28,33 +28,36 @@ func createTables() {
 			email TEXT,
 			password TEXT,
 			image_url TEXT,
+			salt TEXT,
 			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	)`)
+
 	if err != nil {
-		log.Println(err)
+		log.Println(err, "1")
 		return
 	}
 	if _, err := statement.Exec(); err != nil {
-		log.Println(err)
+		log.Println(err, "2")
 		return
 	}
 
 	statement, err = db.Prepare(`
 		CREATE TABLE IF NOT EXISTS posts (
-		id TEXT PRIMARY KEY, 
-		user_id TEXT,
-		FOREGIN KEY (user_id) REFERENCES users(id)
-		title TEXT,
-		content TEXT,
-		image_url TEXT,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+			id TEXT PRIMARY KEY, 
+			user_id TEXT,
+			title TEXT,
+			content TEXT,
+			image_url TEXT,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			is_active BOOLEAN DEFAULT 0,
+			FOREIGN KEY (user_id) REFERENCES users(id)
 	)`)
 	if err != nil {
-		log.Println(err)
+		log.Println(err, "3")
 		return
 	}
 	if _, err := statement.Exec(); err != nil {
-		log.Println(err)
+		log.Println(err, "4")
 		return
 	}
 }
