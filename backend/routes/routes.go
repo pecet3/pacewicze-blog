98557c84ae5b56bee66b2b2a600ded6723f106e9
pecet3 <pecet3107@gmail.com>
2023-11-15 01:05:32 +0100

    new routes for auth

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -14,6 +14,8 @@ func SetupAndRun() {
 
 	mux.HandleFunc("/api/post", controllers.Post).Methods("GET", "POST")
 	mux.HandleFunc("/api/post/{id}", controllers.PostById).Methods("GET", "PUT")
+	mux.HandleFunc("/api/user/register", controllers.Register).Methods("POST")
+	mux.HandleFunc("/api/user/login", controllers.Login).Methods("POST")
 	log.Println("Starting the server at port", port)
 	log.Fatal(http.ListenAndServe(port, mux))
 }
