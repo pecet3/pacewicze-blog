6e325d1524a3dc201c8520efca3bd9f4ed42234a
pecet3 <pecet3107@gmail.com>
2023-11-16 20:40:53 +0100

    validate jwt

--- backend/utils/jwt.go
+++ backend/utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -21,4 +23,39 @@ func CreateJWT() (string, error) {
 	}
 
 	return tokenStr, nil
+}
+
+func ValidateJWT(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenHeader := r.Header.Get("Token")
+
+		if tokenHeader == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("brak autoryzacji"))
+			return
+		}
+
+		token, err := jwt.Parse(tokenHeader, func(t *jwt.Token) (interface{}, error) {
+			_, ok := t.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("brak autoryzacji"))
+				return nil, fmt.Errorf("brak autoryzacji")
+			}
+			return secret, nil
+		})
+		fmt.Println(token)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("brak autoryzacji: " + err.Error()))
+			return
+		}
+
+		if token.Valid {
+			next(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("brak autoryzacji"))
+		}
+	}
 }
