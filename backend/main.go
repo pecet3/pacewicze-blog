28e064f279ceea17d16b2291965d5df4a65ca4ce
pecet3 <pecet3107@gmail.com>
2023-11-16 20:40:59 +0100

    testing jwt

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"backend/models"
 	router "backend/routes"
+	"backend/utils"
+	"fmt"
 )
 
 func main() {
 	models.Config()
+	token, _ := utils.CreateJWT()
+	fmt.Println(token)
 	router.SetupAndRun()
 }
