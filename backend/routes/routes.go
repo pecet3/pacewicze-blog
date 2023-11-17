24576203511902d53c89abe6e8b2c84340c1501d
pecet3 <pecet3107@gmail.com>
2023-11-17 07:10:13 +0100

    creating post is protected

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -3,24 +3,19 @@ package router
 import (
 	controllers "backend/controllers"
 	"backend/utils"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "super secret area")
-}
-
 func SetupAndRun() {
 	mux := mux.NewRouter()
 	port := ":5000"
 
-	mux.HandleFunc("/test", utils.ValidateJWT(Home)).Methods("GET")
+	mux.HandleFunc("/api/post", controllers.Post).Methods("GET")
+	mux.HandleFunc("/api/post", utils.ValidateJWT(controllers.Post)).Methods("POST")
 
-	mux.HandleFunc("/api/post", controllers.Post).Methods("GET", "POST")
 	mux.HandleFunc("/api/post/{id}", controllers.PostById).Methods("GET", "PUT")
 	mux.HandleFunc("/api/user/register", controllers.Register).Methods("POST")
 	mux.HandleFunc("/api/user/login", controllers.Login).Methods("POST")
