4d369d4ba2fa9f41e8a2c44db6f67a2def238580
pecet3 <pecet3107@gmail.com>
2023-11-18 12:18:30 +0100

    cledaned up

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"backend/models"
 	router "backend/routes"
-	"backend/utils"
-	"fmt"
 )
 
 func main() {
 	models.Config()
-	token, _ := utils.CreateJWT()
-	fmt.Println(token)
 	router.SetupAndRun()
 }
