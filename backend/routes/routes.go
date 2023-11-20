37b41355ad4c012edf3a4716d6382e7dc7fff940
pecet3 <pecet3107@gmail.com>
2023-11-20 22:40:39 +0100

    remioved api from endpoint

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -13,14 +13,14 @@ func SetupAndRun() {
 	mux := mux.NewRouter()
 	port := ":5000"
 
-	mux.HandleFunc("/api/post", controllers.Post).Methods("GET")
-	mux.HandleFunc("/api/post", utils.ValidateJWT(controllers.Post)).Methods("POST")
+	mux.HandleFunc("/post", controllers.Post).Methods("GET")
+	mux.HandleFunc("/post", utils.ValidateJWT(controllers.Post)).Methods("POST")
 
-	mux.HandleFunc("/api/post/{id}", controllers.PostById).Methods("GET")
-	mux.HandleFunc("/api/post/{id}", utils.ValidateJWT(controllers.PostById)).Methods("PUT")
+	mux.HandleFunc("/post/{id}", controllers.PostById).Methods("GET")
+	mux.HandleFunc("/post/{id}", utils.ValidateJWT(controllers.PostById)).Methods("PUT")
 
-	mux.HandleFunc("/api/user/register", controllers.Register).Methods("POST")
-	mux.HandleFunc("/api/user/login", controllers.Login).Methods("POST")
+	mux.HandleFunc("/user/register", controllers.Register).Methods("POST")
+	mux.HandleFunc("/user/login", controllers.Login).Methods("POST")
 
 	log.Println("Starting the server at port", port)
 	log.Fatal(http.ListenAndServe(port, mux))
